ad71d012bd03de7f928c3848bdfe2f48473d0e18
Hung <hnguyen5478069@gmail.com>
2025-05-24 18:14:31 -0700

    udpate

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-class Exp{
-    constructor(){
-        this.exp1 = 'novice';
-        this.exp2 = 'intermediate';
-        this.exp3 = 'advance';
-}
- 
+class Exp {
+  constructor() {
+    this.exp1 = "novice";
+    this.exp2 = "intermediate";
+    this.exp3 = "advance";
+  }
 }
 
-const exp= new Exp.exp1;
+const exp = new Exp.exp1();
